728fa8b2d523a500f8bca92f529b43d00fc110d6
Quasiocculti <q@sed.by>
2023-08-15 11:26:19 +0300

    Fix #3

--- bootstrap/bootstrap.rc.php
+++ bootstrap/bootstrap.rc.php
@@ -14,11 +14,11 @@ Hooks=rc
 
 defined('COT_CODE') or die('Wrong URL');
 
-if ($env['ext'] == 'admin') {
-  Resources::linkFileFooter($cfg['plugins_dir'].'/bootstrap/'.$cfg['plugin']['bootstrap']['back'].'/css/bootstrap.min.css', 'css');
-  Resources::linkFileFooter($cfg['plugins_dir'].'/bootstrap/'.$cfg['plugin']['bootstrap']['back'].'/js/bootstrap.bundle.min.js', 'js');
+if ($env['ext'] == 'admin' && !empty(Cot::$cfg['plugin']['bootstrap']['back'])) {
+  Resources::linkFileFooter(Cot::$cfg['plugins_dir'].'/bootstrap/' . Cot::$cfg['plugin']['bootstrap']['back'].'/css/bootstrap.min.css', 'css');
+  Resources::linkFileFooter(Cot::$cfg['plugins_dir'].'/bootstrap/' . Cot::$cfg['plugin']['bootstrap']['back'].'/js/bootstrap.bundle.min.js', 'js');
 }
-else {
-  Resources::linkFileFooter($cfg['plugins_dir'].'/bootstrap/'.$cfg['plugin']['bootstrap']['front'].'/css/bootstrap.min.css', 'css');
-  Resources::linkFileFooter($cfg['plugins_dir'].'/bootstrap/'.$cfg['plugin']['bootstrap']['front'].'/js/bootstrap.bundle.min.js', 'js');
+elseif (!empty(Cot::$cfg['plugin']['bootstrap']['front'])) {
+  Resources::linkFileFooter(Cot::$cfg['plugins_dir'].'/bootstrap/' . Cot::$cfg['plugin']['bootstrap']['front'].'/css/bootstrap.min.css', 'css');
+  Resources::linkFileFooter($cfg['plugins_dir'].'/bootstrap/' . Cot::$cfg['plugin']['bootstrap']['front'].'/js/bootstrap.bundle.min.js', 'js');
 }
--- bootstrap/bootstrap.setup.php
+++ bootstrap/bootstrap.setup.php
@@ -5,7 +5,7 @@ Code=bootstrap
 Name=[SEDBY] Bootstrap
 Category=misc-ext
 Description=Bootstrap support
-Version=1.01
+Version=1.02
 Date=2023-01-25
 Author=Dmitri Beliavski
 Copyright=&copy; 2023 Seditio.By
@@ -16,8 +16,8 @@ Auth_members=RW
 Lock_members=12345A
 [END_COT_EXT]
 [BEGIN_COT_EXT_CONFIG]
-front=01:string::bootstrap-5.3.0-alpha1-dist:
-back=02:string::bootstrap-5.3.0-alpha1-dist:
+front=01:string::bootstrap-5.3.0-dist:
+back=02:string::bootstrap-5.3.0-dist:
 [END_COT_EXT_CONFIG]
 ==================== */
 
